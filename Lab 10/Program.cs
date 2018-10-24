ad088140d30eec85e5cd76837feb6f29c495fcd4
Jacky Chan <cjacky624@gmail.com>
2018-10-24 00:17:33 -0400

    Lab 10 Circum/Area of Circle

---
+++ Lab 10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double UserInput = 0;
+                Circle myCircle = new Circle(UserInput);
+
+                Console.WriteLine("Please enter the radius of a circle!");
+                UserInput = double.Parse(Console.ReadLine());
+                myCircle.Radius = UserInput;
+
+                Console.WriteLine($"Circumference Formatted!: {myCircle.CalculateFormattedCircumference()}");
+
+                Console.WriteLine($"Area: {myCircle.CalculateFormattedArea(),0:N2}");
+
+                Console.WriteLine("Would you like to continue calculating the circumference and area of a circle? (yes/no)");
+                string userInput2 = Console.ReadLine().ToLower();
+                
+                if (userInput2 != "yes")
+                {
+                    Console.WriteLine("Thank you, goodbye!");
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
